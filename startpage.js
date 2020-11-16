b6ee8bb2151e20fd0cff99111ce8e097d7b7a9a9
oscargregorius <oscar.gregorius@gmail.com>
2020-11-16 11:13:06 +0100

    Fixed when you hover over a div it changes the color

--- startpage.js
+++ startpage.js
@@ -174,25 +174,15 @@ export default class Startpage {
         $(this).css({ zIndex: 100 });
       })
       .on('dragMove', function (e, pointer) {
-        //let { pageX, pageY } = pointer;
+        let { pageX, pageY } = pointer;
 
 
         for (let i = 0; i <= 224; i++) {
-          console.log($(`.squares[data-index = ${i}]`).offset());
-          if ($(this).offset() === $(`.squares[data-index = ${i}]`).offset()) {
-            $(`.squares[data-index = ${i}]`).css("background-color", "red");
+          if (Math.round($(this).offset().left) === Math.round($(`.squares[data-index = ${i}]`).offset().left) &&
+            Math.round($(this).offset().top) === Math.round($(`.squares[data-index = ${i}]`).offset().top)) {
+            $(`.squares[data-index = ${i}]`).css("background-color", "seagreen");
           }
         }
-
-
-
-        /*$('.squares').mouseenter(function () {
-          $(this).css("background-color", "red");
-        });
-        $('.squares').mouseleave(function () {
-          $(this).css("background-color", "#54aa79");
-        });*/
-
 
         // we will need code that reacts
         // if you have moved a tile to a square on the board
